8a5570d379ec41309b1f6b175ea8252033b98de9
Wail Kame <wailkame@gmail.com>
2020-12-22 16:23:15 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+
+@section('content')
+    <div>Index of Movies</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        
+    </head>
+    <body>
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
